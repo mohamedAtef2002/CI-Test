40ecd618b5850e7423424f21f22b272af4c056ea
Mohamed Atef <mohamedatef@ntgclarity.com>
2025-08-20 12:38:51 +0300

    ci test

--- src/test/java/com/example/CI_Test/CiTestApplicationTests.java
+++ src/test/java/com/example/CI_Test/CiTestApplicationTests.java
@@ -3,9 +3,12 @@ package com.example.CI_Test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class CiTestApplicationTests {
 
 	@Test
